bd64cbde8012080572a3f32136a62061405913a4
Yaadee <yadasat437@gmail.com>
2025-01-09 14:32:24 -0500

    redux toolkit added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,7 +6,7 @@ import { Provider } from 'react-redux';
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { useReducer } from './user/userSlice'
+import userReducer from './user/userSlice'
+
 
 export const store = configureStore({
   reducer: {
-    user:useReducer,
+    user:userReducer
   },
+    
 })
